6a1b376a7b534229f90bd2969a8fe2730759a6d6
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-07-16 19:05:21 +0530

    Learn data types

--- 1.py
+++ 1.py
@@ -1,3 +1,13 @@
-print("Hello git and VS code")
-print("Auto save")
-print("Auto save")
+from typing import List
+
+
+List1 = ["Hello1",'Python1',["Hi","Python2"]]
+List2 = ["Hello",'World',10,5.0,List1]
+print(List2[0])
+'''
+a = List2[2] + List2[3]
+print(a)
+'''
+
+List3 = List1 + List2
+print(List3[2][1])
