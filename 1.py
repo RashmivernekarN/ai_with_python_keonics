048a4700aca23a7e3b81ddce39cfef7ab7c74ae8
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-07-16 19:08:22 +0530

    test

--- 1.py
+++ 1.py
@@ -4,10 +4,10 @@ from typing import List
 List1 = ["Hello1",'Python1',["Hi","Python2"]]
 List2 = ["Hello",'World',10,5.0,List1]
 print(List2[0])
-'''
+
 a = List2[2] + List2[3]
 print(a)
-'''
+
 
 List3 = List1 + List2
 print(List3[2][1])
