1691996bc90be4a019f2a52baf6831af639c0c01
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-08-02 17:36:52 +0530

    Control Flow Notes Added

---
+++ 2.py
@@ -0,0 +1,44 @@
+# for i in range(10):
+#     print(i,end=" ")
+    
+#     if i== 2:
+#         print("Break applied")
+#         break
+# print("Outside loop")
+
+
+# for var in 'Python world':
+#     if var == 't':
+#         print('\nskipping t\n')
+#         continue
+#     print(var, end = " ")
+# string1='Python'
+# pi=3.14
+# print(len(string1))
+# string1= 'World\'s curves is defined by ' + str(pi)
+# print(string1)
+
+
+
+
+name=['abc','def','ghi']
+roll_no = [1,4,2]
+
+mapped = zip(roll_no,name)
+print(dict(mapped))
+
+
+
+
+# l1=['andy','apple','sam']
+# s1='Python'
+
+# obj1=enumerate(l1)
+# obj2=enumerate(s1)
+
+# print('Return type:', type(obj1))
+# print(list(enumerate(l1)))
+
+# print(dict(enumerate(l1,2)))
+
+
