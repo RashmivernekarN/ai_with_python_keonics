bdf88a0db8d1537d0025b81a5bbaef0bf5ad3aa5
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-08-30 18:40:39 +0530

    30th aug

--- module2/files.py
+++ module2/files.py
@@ -1,16 +1,17 @@
 # Program to show various ways to read and
 # write data in a file.
 file1 = open("myfile.txt","w")
-L = ["This is Delhi \n","This is Paris \n","This is London \n"]
 
 # \n is placed to indicate EOL (End of Line)
 file1.write("Hello \n")
+L = ["This is Dharwad \n","This is Dharwad \n","This is London \n"]
 file1.writelines(L)
 file1.close() #to change file access modes
+
+
 file1 = open("myfile.txt","r+")
 print("Output of Read function is ")
 print(file1.read())
-print()
 # seek(n) takes the file handle to the nth
 # bite from the beginning.
 file1.seek(0)
@@ -19,15 +20,16 @@ print(file1.readline())
 print()
 file1.seek(0)
 # To show difference between read and readline
-print("Output of Read(9) function is ")
-print(file1.read(9))
-print()
+print("Output of Read(100) function is ")
+print(file1.read(100))
 file1.seek(0)
 print("Output of Readline(9) function is ")
 print(file1.readline(9))
 file1.seek(0)
 # readlines function
 print("Output of Readlines function is ")
-print(file1.readlines())
+print(file1.readlines()[2])
+
+
 print()
-file1.close()
+# file1.close()
